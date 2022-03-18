6aeb63083857f5acee937706233d3a34405e0ba2
Elia Angga <eliangga2002@gmail.com>
2022-03-18 17:32:25 +0700

    first commit

---
+++ tp4/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HaloGeneric<string> halo = new HaloGeneric<string>();
+            String x = "Elia Angga";
+            halo.SapaUser(x);
+            DataGeneric<String> data = new DataGeneric<string>("1302204005");
+            data.PrintData();
+        }
+    }
+
+    public class HaloGeneric<T>
+    {
+        public void SapaUser(T x)
+        {
+            Console.WriteLine("Halo " + x);
+        }
+    }
+
+    public class DataGeneric<T>
+    {
+        private T data;
+        public DataGeneric(T data)
+        {
+            this.data = data;
+        }
+        public void PrintData()
+        {
+            Console.WriteLine("Data yang tersimpan adalah: " + this.data);
+        }
+    }
+}
+
+// Elia Angga - 1302204005
+
